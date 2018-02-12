9c82f3eb468b39559762447ac61201d3e52c3140
rob-now <rnowakowski.prv@gmail.com>
2018-02-12 11:59:21 +0100

    Added code in function4.js

--- function4.js
+++ function4.js
@@ -1,8 +1,18 @@
 console.log("\n---Zadanie 4---");
 /*
-Napisz funkcję, która przyjmie dowolny tekst.
-Funkcja niech zwraca tekst, który ma zmiksowaną wielkość liter np:
-input -> Ala ma kota
-output -> AlA Ma kOtA
+Napisz funkcję, która będzie wymagać 2 atrybutów.
+Funkcja niech sprawdza, czy oba atrybuty są numerami.
+Funkcja ma zwracać iloczyn (*) obu liczb.
+Jeżeli któryś z atrybutów nie jest liczbą, funkcja niech zwraca false.
+NIE Wywołuj na razie tej funkcji
  */
 
+function multiply(val1, val2) {
+    if (typeof(val1) !== "number" || typeof(val2) !== "number") {
+        return false;
+    } else {
+        return val1 * val2;
+    }
+}
+
+console.log(multiply(4,3));
