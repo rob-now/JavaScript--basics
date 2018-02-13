7fab10a72d348b82cee443305e233b4511a416eb
rob-now <rnowakowski.prv@gmail.com>
2018-02-13 12:32:27 +0100

    Renamed arrays1.js -> arrays0.js and added arrays1.js

--- arrays1.js
+++ arrays1.js
@@ -1,14 +1,25 @@
-console.log("\n---Zadanie 0---");
+console.log("\n---Zadanie 1---");
 
 /*
-Podlicz wszystkie litery w tablicy words
+Stwórz tablicę 10 losowych absolutnych liczb z przedziału 1-10.
+
+Po utworzeniu tablicy wypisz w konsoli jej największy i najmniejszy element.
+Uwaga! Nie stosuj do tego pętli for a metodę sort()
  */
 
-const words = ["kot", "świnka", "pies", "chomik", "wróbelek", "szczupakko"];
+const min = 1;
+const max = 10;
+var arr = [];
 
-var count = 0;
-for (var i = 0; i < words.length; i++) {
-    count += words[i].length;
+for (var i = 0; i < 10; i++) {
+    arr.push(Math.floor(Math.random() * (max - min + 1) + min));
+    //console.log(arr);
 }
+console.log("arr:", arr);
 
-console.log("\nNumber of letters:", count);
+arr.sort(function(a, b){
+    return a - b;
+});
+console.log("arr sorted:", arr);
+console.log("min:", arr[0]);
+console.log("max:", arr[arr.length-1]);
